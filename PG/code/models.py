72236ae71d4440cc7169fcdc01d020bc5ffb1202
tankmanbeta <1148099884@qq.com>
2024-01-26 19:34:53 +0800

    Add implementation of PG

---
+++ PG/code/models.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+
+"""
+    @Author 坦克手贝塔
+    @Date 2024/1/26 15:24
+"""
+from collections import deque, namedtuple
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import optim
+from torch.distributions import Categorical
+
+DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
+
+
+# use namedtuple to help manager transitions
+Transition = namedtuple("Transition", ("state", "action", "reward", "next_state", "action_prob", "done"))
+
+
+class Data:
+    def __init__(self):
+        self.pool = deque([])
+
+    def get_data(self):
+        state_list, action_list, reward_list, state_next_list, action_prob_list, done_list = [], [], [], [], [], []
+        for transition in self.pool:
+            state, action, reward, state_next, action_prob, done = transition
+            state_list.append(state)
+            action_list.append(action)
+            reward_list.append(reward)
+            state_next_list.append(state_next)
+            action_prob_list.append(action_prob)
+            done_list.append(done)
+        self.clear()
+        data = {
+            "state": state_list,
+            "action": action_list,
+            "reward": reward_list,
+            "state_next": state_next_list,
+            "action_prob": action_prob_list,
+            "done": done_list
+        }
+        return data
+
+    def push(self, *args):
+        self.pool.append(Transition(*args))
+
+    def clear(self):
+        self.pool = deque([])
+
+    def __len__(self):
+        return len(self.pool)
+
+
+# Policy
+class PolicyNet(nn.Module):
+    def __init__(self, n_features, n_actions, n_width=300, n_depth=2):
+        super(PolicyNet, self).__init__()
+        self.n_features = n_features
+        self.n_actions = n_actions
+        self.n_width = n_width
+        self.n_depth = n_depth
+        self.model = self.build_model()
+
+    def build_model(self):
+        layer_dim = [self.n_features] + [self.n_width for _ in range(self.n_depth)]
+        layers = []
+        for i in range(len(layer_dim) - 1):
+            layers.append(nn.Linear(layer_dim[i], layer_dim[i + 1]))
+            layers.append(nn.ReLU())
+        layers.append(nn.Linear(self.n_width, self.n_actions))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        if isinstance(x, np.ndarray):
+            x = torch.FloatTensor(x)
+        out = F.softmax(self.model(x), dim=-1)
+        return out
+
+
+class PGAgent:
+    def __init__(self, n_features, n_actions, gamma=0.98, lr=1e-3):
+        super(PGAgent, self).__init__()
+        self.n_features = n_features
+        self.n_actions = n_actions
+        self.gamma = gamma
+        self.lr = lr
+        self.policy = PolicyNet(n_features, n_actions).to(DEVICE)
+        self.optimizer_policy = optim.Adam(params=self.policy.parameters(), lr=lr)
+
+    # choose action according to the distribution
+    def choose_action(self, state):
+        pi = self.policy(state)
+        dist = Categorical(pi)
+        action = dist.sample().item()
+        action_prob = pi[action].item()
+        return action, action_prob
+
+    def learn(self, transition_dict):
+        reward_list = transition_dict["reward"]
+        state_list = transition_dict["state"]
+        action_list = transition_dict["action"]
+
+        G = 0
+        self.optimizer_policy.zero_grad()
+        for i in reversed(range(len(reward_list))):
+            reward = reward_list[i]
+            state = torch.tensor([state_list[i]], dtype=torch.float).to(DEVICE)
+            action = torch.tensor([action_list[i]]).view(-1, 1).to(DEVICE)
+            log_prob = torch.log(self.policy(state).gather(1, action))
+            G = self.gamma * G + reward
+            loss = -log_prob * G
+            loss.backward()
+        self.optimizer_policy.step()
+
+    def save_checkpoint(self, save_path, episode):
+        torch.save(self.policy.state_dict(), f"{save_path}/{episode}-model.pkl")
---
+++ PG/code/utils.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+
+"""
+    @Author 坦克手贝塔
+    @Date 2024/1/26 15:24
+"""
+
+from datetime import datetime
+from pathlib import Path
+
+import gym
+import matplotlib.pyplot as plt
+import torch
+from models import PGAgent, Data
+from torch.utils.tensorboard import SummaryWriter
+
+
+def run_one_episode(env, agent, data):
+    state, info = env.reset()
+    reward_episode = 0
+    done = False
+    count = 0
+    while count < 200 and not done:
+        action, action_prob = agent.choose_action(state)
+        state_next, reward, done, _, _ = env.step(action)
+        data.push(state, action, reward, state_next, action_prob, done)
+        state = state_next
+        reward_episode += reward
+        count += 1
+        if done:
+            break
+    data = data.get_data()
+    agent.learn(data)
+    return reward_episode
+
+
+def evaluate(env, agent, save_path):
+    state, info = env.reset()
+    reward_episode = 0
+    frame_list = []
+    done = False
+    count = 0
+    while count < 200 and not done:
+        prob = agent.policy(state)
+        action = torch.argmax(prob).item()
+        next_state, reward, done, _, _ = env.step(action)
+        reward_episode += reward
+        state = next_state
+        count += 1
+        frame_list.append(env.render())
+    # draw frames
+    for idx, frame in enumerate(frame_list):
+        plt.imshow(frame)
+        plt.axis('off')
+        plt.savefig(f"{save_path}/{idx}.png")
+        plt.show()
+
+
+def train():
+    print("Training starts!!!")
+    now = datetime.now().strftime("%Y%m%d%H%M%S")
+    prefix_path = Path(f"../results/{now}")
+    prefix_path.mkdir(exist_ok=True)
+    log_path = prefix_path / "logs"
+    log_path.mkdir(exist_ok=True)
+    pic_path = prefix_path / "pics"
+    pic_path.mkdir(exist_ok=True)
+    checkpoint_path = prefix_path / "checkpoints"
+    checkpoint_path.mkdir(exist_ok=True)
+    writer = SummaryWriter(log_dir=log_path)
+    env_name = "CartPole-v1"
+    env = gym.make(env_name, render_mode="rgb_array")
+    observation_n, action_n = env.observation_space.shape[0], env.action_space.n
+    agent = PGAgent(observation_n, action_n, gamma=0.98, lr=2e-3)
+    reward_list = []
+    data = Data()
+    for episode in range(100):
+        reward = run_one_episode(env, agent, data)
+        reward_list.append(reward)
+        print(f"Episode: {episode}, reward: {reward}")
+        writer.add_scalar("Reward/train", reward, global_step=episode)
+        if (episode + 1) % 10 == 0:
+            agent.save_checkpoint(checkpoint_path, episode)
+    writer.close()
+    plt.plot(list(range(len(reward_list))), reward_list)
+    plt.show()
+    print("Training ends!!!")
+    evaluate(env, agent, pic_path)
